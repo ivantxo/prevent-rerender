2414cc26df5ce4a736c5a2351b231b1d96fa298c
Ivan <ivan.ramirezf@gmail.com>
2019-03-12 15:09:21 +1000

    Prevent re-render on child components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, PureComponent } from 'react';
+import styled from 'styled-components';
+
+const list = new Array(5000).fill(0).map((v, i) => i);
 
 class App extends Component {
+  state = {
+    perspective: false,
+  };
+
+  togglePerspective = () => {
+    this.setState(state => ({ perspective: !state.perspective }));
+  };
+  
   render() {
+    console.log('render app');
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Button onClick={this.togglePerspective}>
+          Toggle Perspective
+        </Button>
+
+        <Perspective>
+          {list.map(v => <Square key={v} number={v} />)}
+        </Perspective>
       </div>
     );
   }
 }
+
+const Perspective = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: ${props => (props.perspective ? 'row' : 'column')};
+`;
+
+const Button = ({ onClick, children }) =>
+  console.log('render button') || (
+  <button type="button" onClick={onClick}>
+    {children}
+  </button>
+);
+
+class Square extends PureComponent {
+  render() {
+    console.log('render square');
+    return <Item>{this.props.number * this.props.number}</Item>;
+  }
+}
+
+// Using the shouldComponentUpdate life cycle avoids this child component to be re-rendered
+// class Square extends Component {
+//   shouldComponentUpdate(nextProps, nextState) {
+//     if (this.props.number === nextProps.number) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+
+//   render() {
+//     console.log('render square');
+//     return <Item>{this.props.number * this.props.number}</Item>;
+//   }
+// }
+
+// If using a stateles component it renders 5000 times every time we press Toggle Perspective
+// const Square = ({ number }) => console.log('render square') || <Item>{number * number}</Item>;
+
+const Item = styled.div`
+  margin: 10px;
+`;
 
 export default App;
